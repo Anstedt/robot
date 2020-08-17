1019c69e41385cd8271617d127b23d1ad827473e
Howard J Anstedt <anstedt@gmail.com>
2020-08-17 11:27:32 -0500

    Fixed init issues with variables inlcuding currentTime

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 # MPU6050 for balancing
 
+- Seems like something needs to be configured since the pitch as well
+  as other values work sometimes but other times do not.
+- The range always seems off somehow as well.
+
 # DRV8825 for motor control
--- mpu6050/mpu6050.c
+++ mpu6050/mpu6050.c
@@ -23,7 +23,7 @@
 #define GYRO_XOUT_H  0x43
 #define GYRO_YOUT_H  0x45
 #define GYRO_ZOUT_H  0x47
-#define PWR_MGMT_1   0x6B	
+#define PWR_MGMT_1   0x6B
 
 float AccX,AccY,AccZ;
 float GyroX,GyroY,GyroZ;
@@ -42,9 +42,11 @@ int fd;
 
 short read_raw_data(int addr){
 	short high_byte,low_byte,value;
-	high_byte = wiringPiI2CReadReg8(fd, addr);
-	low_byte = wiringPiI2CReadReg8(fd, addr+1);
-	value = (high_byte << 8) | low_byte;
+  //	high_byte = wiringPiI2CReadReg8(fd, addr);
+	// low_byte = wiringPiI2CReadReg8(fd, addr+1);
+	// value = (high_byte << 8) | low_byte;
+  value = wiringPiI2CReadReg16(fd, addr);
+  value = ((value<<8)&0xff00)|((value>>8)&0x00ff);
 	return value;
 }
 
@@ -58,11 +60,17 @@ void calculate_IMU_error() {
   // We can call this funtion in the setup section to calculate the accelerometer and gyro data error. From here we will get the error values used in the above equations printed on the Serial Monitor.
   // Note that we should place the IMU flat in order to get the proper values, so that we then can the correct values
   // Read accelerometer values 200 times
+  AccErrorX = 0.0;
+  AccErrorY = 0.0;
+  GyroErrorX = 0.0;
+  GyroErrorY = 0.0;
+  GyroErrorZ = 0.0;
+
   while (c < 200) {
 		/*Read raw value of Accelerometer and gyroscope from MPU6050*/
-		AccX = read_raw_data(ACCEL_XOUT_H) / 16384.0;
-		AccY = read_raw_data(ACCEL_YOUT_H) / 16384.0;
-		AccZ = read_raw_data(ACCEL_ZOUT_H) / 16384.0;
+		AccX = read_raw_data(ACCEL_XOUT_H) / (16384.0/2); // 4g
+    AccY = read_raw_data(ACCEL_YOUT_H) / (16384.0/2); // 4g
+    AccZ = read_raw_data(ACCEL_ZOUT_H) / (16384.0/2); // 4g
     // Sum all readings
     AccErrorX = AccErrorX + ((atan((AccY) / sqrt(pow((AccX), 2) + pow((AccZ), 2))) * 180 / M_PI));
     AccErrorY = AccErrorY + ((atan(-1 * (AccX) / sqrt(pow((AccY), 2) + pow((AccZ), 2))) * 180 / M_PI));
@@ -75,8 +83,8 @@ void calculate_IMU_error() {
   // Read gyro values 200 times
   while (c < 200) {
     GyroX = read_raw_data(GYRO_XOUT_H);
-		GyroY = read_raw_data(GYRO_YOUT_H);
-		GyroZ = read_raw_data(GYRO_ZOUT_H);
+    GyroY = read_raw_data(GYRO_YOUT_H);
+    GyroZ = read_raw_data(GYRO_ZOUT_H);
 
     // Sum all readings
     GyroErrorX = GyroErrorX + (GyroX / 131.0);
@@ -92,31 +100,46 @@ void calculate_IMU_error() {
   printf("M_PI:%.3f AEX:%.3f AEY:%.3f GyroErrorX:%.3f GyroErrorY:%.3f GyroErrorZ:%.3f\n", M_PI, AccErrorX, AccErrorY,GyroErrorX, GyroErrorY, GyroErrorZ);
 }
 
-void MPU6050_Init(){	
-	wiringPiI2CWriteReg8 (fd, PWR_MGMT_1, 0x00);	// Write to power management register
-	// wiringPiI2CWriteReg8 (fd, ACCEL_CONFIG, 0x10);	// Write to Accel
-	// wiringPiI2CWriteReg8 (fd, GYRO_CONFIG, 0x10);	// Write to Gyro Configuration register
-	// wiringPiI2CWriteReg8 (fd, SMPLRT_DIV, 0x07);	/* Write to sample rate register */
-	// wiringPiI2CWriteReg8 (fd, CONFIG, 0);		/* Write to Configuration register */
-	// wiringPiI2CWriteReg8 (fd, INT_ENABLE, 0x01);	/*Write to interrupt enable register */
+void MPU6050_Init(){
+  // wiringPiI2CWriteReg8 (fd, PWR_MGMT_1, 0x80); // Write to power management register
+  // delay(100);
+  // wiringPiI2CWriteReg8 (fd, 0x68, 0x07);
+  // delay(100);
+  // wiringPiI2CWriteReg8 (fd, PWR_MGMT_1, 0x00); // Write to power managementregister
+
+  wiringPiI2CWriteReg8 (fd, PWR_MGMT_1, 0x00);  // Write to power management register
+
+  wiringPiI2CWriteReg8 (fd, GYRO_CONFIG, 0x00); // Write to Gyro Configuration register
+  wiringPiI2CWriteReg8 (fd, ACCEL_CONFIG, 0x08);  // Write to Accel
+
+  // wiringPiI2CWriteReg8 (fd, SMPLRT_DIV, 0x07); /* Write to sample rate register */
+  wiringPiI2CWriteReg8 (fd, CONFIG, 0x03);    /* Write to Configuration register */
+  // wiringPiI2CWriteReg8 (fd, INT_ENABLE, 0x01); /*Write to interrupt enable register */
   calculate_IMU_error();
   delay(20);
-	} 
+  }
 
 int main(){
-	fd = wiringPiI2CSetup(Device_Address);   /*Initializes I2C with device Address*/
-	MPU6050_Init();		                 /* Initializes MPU6050 */
-	
-	while(1)
-	{
-		/*Read raw value of Accelerometer and gyroscope from MPU6050*/
-		AccX = read_raw_data(ACCEL_XOUT_H) / 16384.0;
-		AccY = read_raw_data(ACCEL_YOUT_H) / 16384.0;
-		AccZ = read_raw_data(ACCEL_ZOUT_H) / 16384.0;
-		
+  fd = wiringPiI2CSetup(Device_Address);   /*Initializes I2C with device Address*/
+  MPU6050_Init();                    /* Initializes MPU6050 */
+  float yawsv = 0.0;
+  yaw = 0.0;
+  roll = 0.0;
+  pitch = 0.0;
+  elapsedTime = 0.0;
+  currentTime = millis();
+  previousTime = 0.0;
+
+  while(1)
+  {
+    /*Read raw value of Accelerometer and gyroscope from MPU6050*/
+    AccX = read_raw_data(ACCEL_XOUT_H) / (16384.0/2); // 4g
+    AccY = read_raw_data(ACCEL_YOUT_H) / (16384.0/2); // 4g
+    AccZ = read_raw_data(ACCEL_ZOUT_H) / (16384.0/2); // 4g
+
     // Calculating Roll and Pitch from the accelerometer data
-    accAngleX = (atan(AccY / sqrt(pow(AccX, 2) + pow(AccZ, 2))) * 180 / M_PI) - 0.58; // AccErrorX ~(0.58) See the calculate_IMU_error()custom function for more details
-    accAngleY = (atan(-1 * AccX / sqrt(pow(AccY, 2) + pow(AccZ, 2))) * 180 / M_PI) + 1.58; // AccErrorY ~(-1.58)
+    accAngleX = (atan(AccY / sqrt(pow(AccX, 2) + pow(AccZ, 2))) * 180 / M_PI) + 5.920; // AccErrorX ~(0.58) See the calculate_IMU_error()custom function for more details
+    accAngleY = (atan(-1 * AccX / sqrt(pow(AccY, 2) + pow(AccZ, 2))) * 180 / M_PI) - 1.608; // AccErrorY ~(-1.58)
 
     // === Read gyroscope data === //
     previousTime = currentTime;        // Previous time is stored before the actual time read
@@ -124,27 +147,30 @@ int main(){
     elapsedTime = (currentTime - previousTime) / 1000; // Divide by 1000 to get seconds
 
     GyroX = read_raw_data(GYRO_XOUT_H) / 131.0;
-		GyroY = read_raw_data(GYRO_YOUT_H) / 131.0;
-		GyroZ = read_raw_data(GYRO_ZOUT_H) / 131.0;
+    GyroY = read_raw_data(GYRO_YOUT_H) / 131.0;
+    GyroZ = read_raw_data(GYRO_ZOUT_H) / 131.0;
 
     // Correct the outputs with the calculated error values
-    GyroX = GyroX + 0.069; // GyroErrorX ~(-0.56)
-    GyroY = GyroY + 0.081; // GyroErrorY ~(2)
-    GyroZ = GyroZ + 0.093; // GyroErrorZ ~ (-0.8)
+    GyroX = GyroX - GyroErrorX; // GyroErrorX ~(-0.56)
+    GyroY = GyroY - GyroErrorY; // GyroErrorY ~(2)
+    GyroZ = GyroZ - GyroErrorZ; // GyroErrorZ ~ (-0.8)
 
     // Currently the raw values are in degrees per seconds, deg/s, so we need to multiply by sendonds (s) to get the angle in degrees
     gyroAngleX = gyroAngleX + GyroX * elapsedTime; // deg/s * s = deg
     gyroAngleY = gyroAngleY + GyroY * elapsedTime;
+    yawsv = yaw;
     yaw =  yaw + GyroZ * elapsedTime;
+    // printf("yawsv=%.3f \tyaw=%.3f \tGyroX=%.3f \telapsed=%.3f\n", yawsv, yaw, GyroX, elapsedTime);
 
     // Complementary filter - combine acceleromter and gyro angle values
     roll = 0.96 * gyroAngleX + 0.04 * accAngleX;
     pitch = 0.96 * gyroAngleY + 0.04 * accAngleY;
 
-    printf("roll=%.3f \tpitch=%.3f \tyaw=%.3f\n", roll ,pitch, yaw);
-		// printf("\n Gx=%.3f °/s\tGy=%.3f °/s\tGz=%.3f °/s\tAx=%.3f g\tAy=%.3f g\tAz=%.3f g\n",Gx,Gy,Gz,Ax,Ay,Az);
-		delay(100);
-		
-	}
+    // printf("AccX=%.3f \tAccY=%.3f \tAccZ=%.3f\n", AccX, AccY, AccZ);
+    // printf("GyroX=%.3f \tGyroY=%.3f \tGyroZ=%.3f\n", GyroX, GyroY, GyroZ);
+    printf("roll=%.3f \tpitch=%.3f \tyaw=%.3f \telapsed=%.3f\n", roll ,pitch, yaw, elapsedTime);
+    // printf("\n Gx=%.3f °/s\tGy=%.3f °/s\tGz=%.3f °/s\tAx=%.3f g\tAy=%.3f g\tAz=%.3f g\n",Gx,Gy,Gz,Ax,Ay,Az);
+    delay(100);
+  }
 	return 0;
 }
