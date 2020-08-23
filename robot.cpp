c183690fb09c16d9f59c132c383cc9fb27a8a659
Howard J Anstedt <anstedt@gmail.com>
2020-08-23 12:04:30 -0500

    Default tes Gyro thread for now

--- robot.cpp
+++ robot.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include "mpu6050/mpu6050.h"
-#include "gyro/Gyro.h"
+// #include "gyro/Gyro.h"
 
 int main(int argc, char *argv[])
 {
   std::cout << "Hello Howie" << std::endl;
 
-  Gyro gyro;
+  // Gyro gyro;
 
-  gyro.Activate();
+  // gyro.Activate();
   
   // Run the mpu6050 and capture the return
   return(mpu_run());
