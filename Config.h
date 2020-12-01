1a14636f1a050a1af8ef89db7e717dd6ec285d04
Howard J Anstedt <anstedt@gmail.com>
2020-11-30 18:55:22 -0600

    Fixed wrong GPIO for motor 1 direction

--- Config.h
+++ Config.h
@@ -12,7 +12,7 @@ const int MOTORS_MODE_DEFAULT  = 5;
 const int MOTORS_RPM_DEFAULT   = 1;
 
 const int MOTOR1_GPIO_STEP     = 10; // 21
-const int MOTOR1_GPIO_DIR      = 23; // 20
+const int MOTOR1_GPIO_DIR      = 18; // 20
 const int MOTOR1_GPIO_MODE_0   = 17; // 14
 const int MOTOR1_GPIO_MODE_1   = 27; // 15
 const int MOTOR1_GPIO_MODE_2   = 22; // 18
