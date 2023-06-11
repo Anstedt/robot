46dccc142ce3cd7ca5c2129b079e3885396fad83
Howard J. Anstedt <anstedt@gmail.com>
2023-06-11 13:42:48 -0500

    Use termio rather than sleep for timing of receive

--- pico/MotorsDriver.cpp
+++ pico/MotorsDriver.cpp
@@ -28,18 +28,59 @@ int main()
   char read_buf [256];
   int serial_port = open("/dev/ttyACM0", O_RDWR);
 
+  struct termios tty;
+  
+  
   if (serial_port < 0)
   {
     printf("Error %i from open: %s\n", errno, strerror(errno));
   }
   else
   {
+    // Init
+    if(tcgetattr(serial_port, &tty) != 0)
+    {
+      printf("Error %i from tcgetattr: %s\n", errno, strerror(errno));
+      return 1;
+    }
+
+    /*
+      tty.c_cflag &= ~PARENB; // Clear parity bit, disabling parity (most common)
+      tty.c_cflag &= ~CSTOPB; // Clear stop field, only one stop bit used in communication (most common)
+      tty.c_cflag &= ~CSIZE; // Clear all bits that set the data size 
+      tty.c_cflag |= CS8; // 8 bits per byte (most common)
+      tty.c_cflag &= ~CRTSCTS; // Disable RTS/CTS hardware flow control (most common)
+      tty.c_cflag |= CREAD | CLOCAL; // Turn on READ & ignore ctrl lines (CLOCAL = 1)
+
+      tty.c_lflag &= ~ICANON;
+      tty.c_lflag &= ~ECHO; // Disable echo
+      tty.c_lflag &= ~ECHOE; // Disable erasure
+      tty.c_lflag &= ~ECHONL; // Disable new-line echo
+      tty.c_lflag &= ~ISIG; // Disable interpretation of INTR, QUIT and SUSP
+      tty.c_iflag &= ~(IXON | IXOFF | IXANY); // Turn off s/w flow ctrl
+      tty.c_iflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL); // Disable any special handling of received bytes
+
+      tty.c_oflag &= ~OPOST; // Prevent special interpretation of output bytes (e.g. newline chars)
+      tty.c_oflag &= ~ONLCR; // Prevent conversion of newline to carriage return/line feed
+      // tty.c_oflag &= ~OXTABS; // Prevent conversion of tabs to spaces (NOT PRESENT ON LINUX)
+      // tty.c_oflag &= ~ONOEOT; // Prevent removal of C-d chars (0x004) in output (NOT PRESENT ON LINUX)
+      */
+
+    tty.c_cc[VTIME] = 1;    // Wait for up to 1s (10 deciseconds), returning as soon as any data is received.
+    tty.c_cc[VMIN] = 20;
+
+    // Save tty settings, also checking for error
+    if (tcsetattr(serial_port, TCSANOW, &tty) != 0)
+    {
+      printf("Error %i from tcsetattr: %s\n", errno, strerror(errno));
+    }
+
     printf("Serial Port opened with FH %d\n", serial_port);
 
     write(serial_port, msg, sizeof(msg));
     fsync(serial_port);
 
-    sleep(1); // Hack to give PICO time to send, real method uses termios to
+    //sleep(1); // Hack to give PICO time to send, real method uses termios to
               // control timing
     
     memset(&read_buf, '\0', sizeof(read_buf));
