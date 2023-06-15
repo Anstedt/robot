194873b6c461e89e6d2e997185deb6bd4e24f60b
Howard J. Anstedt <anstedt@gmail.com>
2023-06-15 17:26:21 -0500

    Final cleanup of LED cycle test

--- pico/PICO-PIO.py
+++ pico/PICO-PIO.py
@@ -2,13 +2,17 @@ from machine import Pin
 from rp2 import PIO, StateMachine, asm_pio
 from time import sleep
 
-@asm_pio(out_init=(rp2.PIO.OUT_HIGH,) * 1, out_shiftdir=PIO.SHIFT_RIGHT, autopull=True, pull_thresh=16)
-def paral_prog():
+# Select 1 bit to use from OUT stream, the in stream for PIO
+# Select shift direction
+# A default wrap occurs for this case so no loop is needed
+# This simply reads the input from the OUT fifo and sends that bit to the LED
+@asm_pio(out_init=(rp2.PIO.OUT_HIGH,) * 1, out_shiftdir=PIO.SHIFT_RIGHT, autopull=True)
+def led_control():
   pull()
   out(pins, 1)
 
 # GPIO PIN 25 should be the onboard LED for the PICO non-W
-sm = StateMachine(0, paral_prog, freq=10000000, out_base=Pin(25))
+sm = StateMachine(0, led_control, freq=10000000, out_base=Pin(25))
 sm.active(1)
 
 while True:
@@ -16,3 +20,5 @@ while True:
   sleep(0.2)
   sm.put(0)
   sleep(0.2)
+
+
