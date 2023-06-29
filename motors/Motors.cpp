12575cf51e08157af15c5f80d214e14352e5a772
Howard J. Anstedt <anstedt@gmail.com>
2023-06-29 17:56:45 -0500

    Start adding interfaces for the robot

--- motors/Motors.cpp
+++ motors/Motors.cpp
@@ -6,19 +6,21 @@ PURPOSE: Interface to PICO for motor control
 *******************************************************************************/
 
 /* NOTES ***********************************************************************
-g++ Motors.cpp -o Motors -lpigpio
+clear ; g++ Motors.cpp -o Motors -l pigpio -I ../sys -I ../ -I .
 sudo ./Motors
 *******************************************************************************/
 
 /* INCLUDE ********************************************************************/
 #include "Motors.h"
 #include <cstdio>
-#include<unistd.h>
+#include <unistd.h>
 #include <cstring>
 #include <pigpio.h>
+#include "Config.h"
 
 /* CLASSES ********************************************************************/
 /* FUNCTIONS ******************************************************************/
+// HJAPID m_pid(&m_input_degrees, &m_output_speed, &m_setpoint, PID_Kp, PID_Ki, PID_Kd, DIRECT)
 Motors::Motors()
 {
   char tty[] = "/dev/ttyACM0";
@@ -56,6 +58,69 @@ Motors::Motors()
     printf("ERROR: serial=%s", serial);
 
   gpioTerminate();
+}
+
+bool Motors::AddGyroData(int y, int x, float angle_gyro, float angle_acc)
+{
+  unsigned int speed; // The pulses per second for the motors, using angle AND mode
+  int distance; // Distance based on speed, mode, as well as 250Hz thread rate
+   
+  // HJA SLOG << "Angle Gyro=" << angle_gyro << "\tAngle Accel=" << angle_acc << "\tGyro Y=" << y << "\tGyro X=" << x << std::endl;
+  
+  // Find the speed we need, then the distance
+  speed = AngleToSpeed(angle_gyro, &distance);
+  
+  SendCmd(speed, distance); // Send the speed and distance, really direction, to PICO
+
+  return(true);
+}
+
+bool Motors::SendCmd(int speed, int distance)
+{
+  int dir = 0;
+
+  if (distance = 0)
+    speed = 0;
+  if (distance > 0)
+    dir = 1;
+  else
+    dir = 0;
+  
+  // Now build strings for PICO
+  return(true);
+}
+
+
+u32 Motors::AngleToSpeed(float angle, s32* distance)
+{
+  bool dist_reverse = false;
+  u32 speed = 0;
+  
+  m_input_degrees = angle; // This has a range of +/-180
+  m_setpoint = 0; // HJA really never changes but we make sure it is set here
+    
+  // After everything is setup in the constructor we are ready to go
+  // HJAPID m_pid.Compute();
+
+  // In our system speed is always positive, since distance handles direction
+  if (m_output_speed < 0)
+  {
+    m_output_speed *= -1;
+
+    // If the speed was negative distance needs to be negative
+    dist_reverse = true;
+  }
+
+  speed = m_output_speed;
+  
+  // Make sure the driver has enough distance even if we are 2 periods late
+  *distance = (speed / PRIMARY_THREAD_RATE) * 2;
+
+  // negate distance if speed was negative, note at this point speed is positive
+  if (dist_reverse)
+    *distance *= -1;
+  
+  return(speed);
 }
 
 int main()
--- motors/Motors.h
+++ motors/Motors.h
@@ -6,7 +6,7 @@ PURPOSE: Interface to PICO for motor control
 *******************************************************************************/
 
 /* NOTES ***********************************************************************
-g++ Motors.cpp -o Motors -lpigpio
+clear ; g++ Motors.cpp -o Motors -l pigpio -I ../sys -I ../ -I .
 sudo ./Motors
 *******************************************************************************/
 
@@ -14,6 +14,7 @@ sudo ./Motors
 #define MOTORS_H
 
 /* INCLUDE ********************************************************************/
+#include "PID_v1.h"
 
 /* CLASSES ********************************************************************/
 /*------------------------------------------------------------------------------
@@ -28,7 +29,13 @@ public:
   // Constructors
   Motors();
   // Mutators: non-const operations
+  bool SendCmd(int speed, int distance);
+  bool AddGyroData(int pitch, int yaw, float angle_acc, float angle_gyro);
+  bool SetMotorsMode(int mode);
+  bool Move(int direction, unsigned int speed);
+  bool Turn(int degrees); // Cause robot to rotate, +/- degrees specifies  direction of rotation
   // Accessors: const operations
+  unsigned int AngleToSpeed(float angle, int* distance);
   // Static and friend functions
   // Memory management: copy constructor, destructor, operator=
   ~Motors() {};
@@ -42,6 +49,35 @@ private:
   Motors(const Motors&);
   Motors& operator=(const Motors& rhs);
   // Data fields
+  // Input: The variable we're trying to control (double)
+  // Output: The variable that will be adjusted by the pid (double)
+  // Setpoint: Destination in out case which is 0
+  // Kp, Ki, Kd: Tuning Parameters. these affect how the pid will change the output. (double>=0)
+  //
+  // kP is gain, basically how hard will we correct the error. I think if the
+  // gain is 1.0 it works just like the system I have right now. Basically the
+  // speed is directly proportional to the distance from 0 degrees we are. We
+  // see this that as we sit the robot up the speed instantly decreases. If we
+  // lay it down the speed instantly speeds up. I think that a lower gain, lets
+  // say 0.5 would mean the speed would more slowly go up till we hit max speed
+  // again.
+
+  // Ki is the integration, this will sum the error over time and is used to
+  // increase the speed if we are not getting to our endpoint or not getting
+  // there fast enough.
+
+  // Kd is the derivative. It helps with overshoot.
+  
+  // Direction: Either DIRECT or REVERSE. determines which direction the output will move when faced with a given error. DIRECT is most common.
+  // PID(input, output, set_point, Kp, Ki, Kd, DIRECT);
+  // HJAPID PID m_pid;
+
+  double m_input_degrees, m_output_speed, m_setpoint;
+  
+  int m_motor1_dir = 0;
+  int m_motor2_dir = 0;
+  int m_motor1_speed = 0;
+  int m_motor2_speed = 0;  
   // Static (shared) class variables
 };
 
